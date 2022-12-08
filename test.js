a4f719ab6f0aaf4d6332bc8ccd3af4fa202f48bd
Henrique <henrique@oertecnologia.com.br>
2022-12-08 12:29:58 -0300

    General Bots 07/12/2022

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 console.log("Feel free to PR refactored and unit tests added if you want to."); /* pronto */ 
+=======
+console.log("Feel free to PR refactored and unit tests added if you want to."); /* Teste */
+>>>>>>> 8146929 (Post General Bot)
